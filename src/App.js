15f47be33a3f094c5e7ea367b702ca9aa41ac763
Johana <jeidylopez@outlook.es>
2024-04-04 08:10:27 -0500

    Client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./assets/Components/Header";
+import Grupos from "./assets/Components/Pages/Grupos";
+
+const TABS = ["Amigos", "Gastos", "Grupos"];
 
 function App() {
+  const [active, setActive] = useState(TABS[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header active={active} setActive={setActive} tabs={TABS} />
+      <p />
+      <p> Página Seleccionada: {active} </p>
+      <Grupos valor={45000}/>
+    </>
   );
 }
 
